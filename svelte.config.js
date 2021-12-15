102a1441a8f6707995c725e3c359a195b21b87c1
Azmayen Murshid <azmayen.murshid@gmail.com>
2021-12-15 14:30:02 +0000

    Debugging the configuration of Gitpod & localhost.

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import { isPropertyAccessChain } from 'typescript';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
